196daace2631876534d6f4840e04006bf0788dd7
Zack <lorenx@163.com>
2025-09-28 19:49:36 +0800

    chore: remove @raypx/supabase package and update API to use @raypx/db; refactor database access in web app

--- apps/web/src/app/api/v2/route.ts
+++ apps/web/src/app/api/v2/route.ts
@@ -1,8 +1,8 @@
-import { db } from "@raypx/supabase";
+import { db, schemas } from "@raypx/db";
 
 export async function GET() {
   const start = Date.now();
-  const { data } = await db.from("user").select("*");
+  const data = await db.select().from(schemas.user);
   const end = Date.now();
   console.log(`Time taken: ${end - start}ms`);
   return Response.json(data);
